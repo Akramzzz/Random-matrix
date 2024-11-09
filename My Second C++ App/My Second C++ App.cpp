01224fdb92072951be6c0703e7fa41d474630742
Akramzzz <abuekerakram@gmail.com>
2024-11-09 22:31:09 +0300

    Added Sum To Columns

--- My Second C++ App/My Second C++ App.cpp
+++ My Second C++ App/My Second C++ App.cpp
@@ -10,8 +10,37 @@
 
 using namespace std;
 
+void FillMatrix(short arr[30][30],short row,short colm) {
 
-void PrintMatrixSum(short sumArr[30], short counter) {
+	for (short x = 0; x < row; x++) {
+		for (short y = 0; y < colm; y++) {
+			arr[x][y] = gnr::RandomNumber(1, 100);
+		}
+	}
+
+
+}
+
+void FillColumnSumArray(short arr[30][30],short sumArr[],short row,short colm) {
+
+	for (short i = 0; i < colm; i++) {
+
+		short colmSum = 0;
+
+		for (short j = 0; j < row; j++) {
+
+			
+			colmSum +=arr[j][i];
+
+		}
+	
+			sumArr[i] = colmSum;
+		
+	}
+
+}
+
+void PrintMatrixRowSum(short sumArr[30], short counter) {
 
 	for (short i = 0; i < counter; i++) {
 
@@ -21,31 +50,56 @@ void PrintMatrixSum(short sumArr[30], short counter) {
 
 }
 
+void PrintMatrixColumnSum(short sumArr[30], short counter){
+
+	for (short i = 0; i < counter; i++) {
+
+		cout << "Sum Of Cloumn " << i + 1 << " : " << sumArr[i] << endl;
+
+	}
+
+
+}
+
 void Matrix(int row, int colm) {
 
 	short arr[30][30];
-	short sumArr[30];
-	short counter = 0;
-
+	short RowSumArr[30];
+	short ColmSumArr[30];
+	
+	FillMatrix(arr, row, colm);
+	
 	for (short i = 0; i < row; i++) {
 
-		short sum = 0;
+		short RowSum = 0;
+		short ColmSum = 0;
 
 		for (short j = 0; j < colm; j++) {
-			arr[i][j] = gnr::RandomNumber(1, 100);
-			sum +=arr[i][j];
+
+			RowSum +=arr[i][j];
+
 			cout << setw(3)<<arr[i][j] << "    ";
-			
+
+
 		}
-		sumArr[counter] = sum;
-		counter++;
+
+		RowSumArr[i] = RowSum;
+
+
 		cout << endl;
 	}
+
+	FillColumnSumArray(arr, ColmSumArr, row, colm);
+
 	cout << "\n\n";
-	PrintMatrixSum(sumArr, counter);
+
+	PrintMatrixRowSum(RowSumArr, row);
+	
+	cout << endl << endl;
+
+	PrintMatrixColumnSum(ColmSumArr,colm);
+
 }
-
-
 
 
 int main()
@@ -60,8 +114,6 @@ int main()
 	cout << "The Following Is A "<<row<<"x"<<colm<< " Random Matrix : \n\n";
 
 	Matrix(row,colm);
-
-
 
 }
 
